14985a3dc372177691dac7622c2e8c4e78ce9855
Khabubu Ndivhuwo <khabubundivhuwo@gmail.com>
2025-05-16 20:21:54 +0200

    feat: enhance RootProviders component with QueryClientProvider and update default theme

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,16 +1,24 @@
+"use client";
 import { ThemeProvider } from "@/components/theme/provider";
-import { Toaster } from "@/components/ui/sonner"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/sonner";
 
-export default function RootProviders({ children }: { children: React.ReactNode }) {
+export default function RootProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="dark"
+      defaultTheme="system"
       enableSystem
       disableTransitionOnChange
     >
-      <Toaster position="top-right" />
-      {children}
+      <QueryClientProvider client={new QueryClient()}>
+        <Toaster position="top-right" richColors />
+        {children}
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
