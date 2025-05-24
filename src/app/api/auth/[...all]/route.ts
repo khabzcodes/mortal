dba94190e1898f2889ff41d9035ee48217011ccd
Khabubu Ndivhuwo <khabubundivhuwo@gmail.com>
2025-05-25 00:58:47 +0200

    refactor(auth): remove manual body parsing configuration

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,7 +1,4 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-// Disallow body parsing, we will parse it manually
-export const config = { api: { bodyParser: false } };
-
 export const { GET, POST } = toNextJsHandler(auth.handler);
